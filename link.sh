07db44f32c4a14811562a27e71b6b4226347ba82
cagrigsby <162646448+cagrigsby@users.noreply.github.com>
2024-09-13 17:10:36 -0500

    Create link.sh

---
+++ link.sh
@@ -0,0 +1,10 @@
+
+
+chmod +x /opt/PopScripts/served.sh; 
+ln -s /opt/PopScripts/served.sh /usr/local/bin/served;
+
+chmod +x /opt/PopScripts/speedysmb.sh;
+ln -s /opt/PopScripts/speedysmb.sh /usr/local/bin/speedysmb;
+
+chmod +x /opt/PopScripts/liggy.sh;
+ln -s /opt/PopScripts/liggy.sh /usr/local/bin/liggy;
