5e6295f3f0a6229d5cada6a1c21a54f7196e5564
cagrigsby <162646448+cagrigsby@users.noreply.github.com>
2024-10-01 11:41:30 -0500

    Update link.sh

--- link.sh
+++ link.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+# ivan
+chmod +x /opt/PopScripts/ivan.sh;
+ln -s /opt/PopScripts/ivan.sh /usr/local/bin/ivan;
+
 # liggy
 chmod +x /opt/PopScripts/liggy.sh;
 ln -s /opt/PopScripts/liggy.sh /usr/local/bin/liggy;
 
-#served
+# served
 chmod +x /opt/PopScripts/served.sh; 
 ln -s /opt/PopScripts/served.sh /usr/local/bin/served;
 
-#speedysmb
+# speedysmb
 chmod +x /opt/PopScripts/speedysmb.sh;
 ln -s /opt/PopScripts/speedysmb.sh /usr/local/bin/speedysmb;
 
